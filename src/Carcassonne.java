16a16b8bbeab18de9124f3953db9916f5c1d171f
Connor Lay <connorlay@maclabcs13.lclark.edu>
2014-01-30 15:24:29 -0800

    An image now draws on the screen! We added the GBC.java file to help with later work.

--- src/Carcassonne.java
+++ src/Carcassonne.java
@@ -7,10 +7,39 @@ public class Carcassonne {
 		System.out.println("Welcome to Carcassonne!");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				JFrame window = new JFrame();
+				GameWindow window = new GameWindow();
 				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				window.setVisible(true);
 			}
 		});
+	}
+}
+
+class GameWindow extends JFrame {
+	public GameWindow() {
+		add(new ImageComponent());
+		pack();
+	}
+}
+
+class ImageComponent extends JComponent {
+
+	private static final int DEFAULT_WIDTH = 150;
+	private static final int DEFAULT_HEIGHT = 150;
+	private Image image;
+
+	ImageComponent() {
+		image = new ImageIcon("00.jpg").getImage();
+	}
+	
+	public void paintComponent(Graphics g){
+		if (image == null){
+			return;
+		}
+		g.drawImage(image, (DEFAULT_WIDTH - image.getWidth(this)) / 2, (DEFAULT_HEIGHT - image.getHeight(this)) / 2, null);
+	}
+	
+	public Dimension getPreferredSize(){
+		return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 	}
 }
