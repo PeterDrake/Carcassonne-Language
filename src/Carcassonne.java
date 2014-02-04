422a45c076df5609dffa1322de86e25165f2b627
Maggie Dreyer <mdreyer@lclark.edu>
2014-02-04 14:21:55 -0800

    Draws start tile at the center of a grid bag layout!

--- src/Carcassonne.java
+++ src/Carcassonne.java
@@ -16,30 +16,15 @@ public class Carcassonne {
 }
 
 class GameWindow extends JFrame {
+	
+	private static final int CENTER = 72;
+	
 	public GameWindow() {
-		add(new ImageComponent());
+		GridBagLayout layout = new GridBagLayout();
+		setLayout(layout);
+		
+		add(new TileImage(new ImageIcon("04.jpg").getImage()), new GBC(CENTER, CENTER));
 		pack();
 	}
-}
-
-class ImageComponent extends JComponent {
-
-	private static final int DEFAULT_WIDTH = 150;
-	private static final int DEFAULT_HEIGHT = 150;
-	private Image image;
-
-	ImageComponent() {
-		image = new ImageIcon("00.jpg").getImage();
-	}
 	
-	public void paintComponent(Graphics g){
-		if (image == null){
-			return;
-		}
-		g.drawImage(image, (DEFAULT_WIDTH - image.getWidth(this)) / 2, (DEFAULT_HEIGHT - image.getHeight(this)) / 2, null);
-	}
-	
-	public Dimension getPreferredSize(){
-		return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-	}
 }
--- src/Tile.java
+++ src/Tile.java
@@ -4,10 +4,9 @@ import javax.swing.*;
 
 public class Tile {
 	
-	private Image image;
 	
-	public Tile(Image image){
-		this.image = image;
+	public Tile(){
+
 	}
 	
 }
