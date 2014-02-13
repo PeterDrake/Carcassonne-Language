348842d5d6d3f4c055e2c2670e87863f9b63d79e
Matisse Hack <matisseh@lclark.edu>
2014-02-13 15:12:25 -0800

    Got it working with more test cases and correct scoring based on whether or not the road is complete/the game is over or not

--- src/Board.java
+++ src/Board.java
@@ -4,15 +4,19 @@ public class Board {
 
 	private Tile[][] board;
 	private Player[] players;
+	private boolean gameOn;
 
 	public Board() {
 		board = new Tile[CENTER * 2][CENTER * 2];
-		board[CENTER][CENTER] = new Tile(CENTER, CENTER, new int[] { 0, 1, 0, 1, 1 }, false);
+		board[CENTER][CENTER] = new Tile(CENTER, CENTER, new int[] { 0, 1, 0,
+				1, 1 }, false);
 
 		players = new Player[5];
 		for (int i = 0; i < players.length; i++) {
 			players[i] = new Player(i);
 		}
+
+		gameOn = true;
 	}
 
 	public void addTile(Tile tile, int r, int c) {
@@ -33,39 +37,46 @@ public class Board {
 		if (!tile.isEnd()) {
 			ScoreTracker currentScore = new ScoreTracker();
 			currentScore.score += 1;
-			
+
 			if ((follower != null) && (follower.getLocationType() == 1)) {
 				// Watch for multiple roads
 				currentScore.followers[follower.getPlayer()] += 1;
 			}
-			
+
 			for (int i = 0; i < tile.SIDES; i++) {
 				if (tile.getSideType(i) == tile.ROAD) {
 					Tile neighbor = getNeighbor(i, tile);
 					if (neighbor != null) {
 						currentScore.score += roadScoreR(currentScore,
 								neighbor, opposite(i));
+					} else {
+						currentScore.isComplete = false;
 					}
 				}
 			}
-			
+
 			divyScore(currentScore);
 		} else {
 			for (int i = 0; i < tile.SIDES; i++) {
-				ScoreTracker currentScore = new ScoreTracker();
-				currentScore.score += 1;
-				
-				if (follower != null && follower.getLocation() == i && follower.getLocationType() == 1) {
-					currentScore.followers[follower.getPlayer()] += 1;
+				if (tile.getSideType(i) == 1) {
+					ScoreTracker currentScore = new ScoreTracker();
+					currentScore.score += 1;
+
+					if (follower != null && follower.getLocation() == i
+							&& follower.getLocationType() == 1) {
+						currentScore.followers[follower.getPlayer()] += 1;
+					}
+
+					Tile neighbor = getNeighbor(i, tile);
+					if (neighbor != null) {
+						currentScore.score += roadScoreR(currentScore,
+								neighbor, opposite(i));
+					} else {
+						currentScore.isComplete = false;
+					}
+
+					divyScore(currentScore);
 				}
-				
-				Tile neighbor = getNeighbor(i, tile);
-				if (neighbor != null) {
-					currentScore.score += roadScoreR(currentScore,
-							neighbor, opposite(i));
-				}
-				
-				divyScore(currentScore);
 			}
 		}
 	}
@@ -83,42 +94,43 @@ public class Board {
 					if (neighbor != null) {
 						return currentScore.score += roadScoreR(currentScore,
 								getNeighbor(i, tile), opposite(i));
+					} else {
+						currentScore.isComplete = false;
 					}
 				}
 			}
-			return 1;
-			// -1 because if there is no road, then the tile must be illegal
 		} else {
-//			currentScore.isComplete = true;
 			if (follower != null && follower.getLocation() == origin
 					&& follower.getLocationType() == 1) {
 				// Should we check location type here or assume it's legal
 				currentScore.followers[follower.getPlayer()] += 1;
 			}
-			return 1;
 		}
+		return 1;
 	}
-	
+
 	public void divyScore(ScoreTracker score) {
-		boolean[] owners = new boolean[5];
-		int max = 1;
-		for (int i = 0; i < score.followers.length; i++) {
-			if (score.followers[i] == max) {
-				owners[i] = true;
-			} else if (score.followers[i] > max) {
-				max = score.followers[i];
-				for (int j = 0; j < owners.length; j++) {
-					owners[j] = false;
+		if (score.isComplete || !gameOn) {
+			boolean[] owners = new boolean[5];
+			int max = 1;
+			for (int i = 0; i < score.followers.length; i++) {
+				if (score.followers[i] == max) {
+					owners[i] = true;
+				} else if (score.followers[i] > max) {
+					max = score.followers[i];
+					for (int j = 0; j < owners.length; j++) {
+						owners[j] = false;
+					}
+					owners[i] = true;
 				}
-				owners[i] = true;
 			}
-		}
-		for (int i = 0; i < owners.length; i++) {
-			if (owners[i]) {
-				addPlayerScore(i, score.score);
+			for (int i = 0; i < owners.length; i++) {
+				if (owners[i]) {
+					addPlayerScore(i, score.score);
+				}
 			}
+			// Not sure if this is a good algorithm
 		}
-		// Not sure if this is a good algorithm
 	}
 
 	public int opposite(int i) {
@@ -145,7 +157,7 @@ public class Board {
 		case 1:
 			return board[tile.getRow()][tile.getCol() + 1];
 		case 2:
-			return board[tile.getRow() +  1][tile.getCol()];
+			return board[tile.getRow() + 1][tile.getCol()];
 		case 3:
 			return board[tile.getRow()][tile.getCol() - 1];
 		default:
@@ -170,4 +182,7 @@ public class Board {
 		tile.placeFollower(player, tile.getSideType(location), location);
 	}
 
+	public void gameOver() {
+		gameOn = false;
+	}
 }
--- src/BoardTest.java
+++ src/BoardTest.java
@@ -14,17 +14,15 @@ public class BoardTest {
 	}
 
 	@Test
-	public void testScoreTile() {
+	public void testScoreTileGameOver() {
+		board.gameOver();
+		
 		int playerUp = 0;
 		int r = board.CENTER;
 		int c = board.CENTER;
 		
 		board.scoreTile(r, c);
 		assertEquals(0, board.getPlayerScore(playerUp));
-		
-//		board.placeFollower(r, c, playerUp, 4);
-//		board.scoreTile(r, c);
-//		assertEquals(1, board.getPlayerScore(playerUp));
 		
 		c += 1;
 		board.addTile(new Tile(r, c, new int[] { 0, 1, 0, 1, 1 }, false), r, c);
@@ -42,6 +40,104 @@ public class BoardTest {
 		board.addTile(new Tile(r, c, new int[] { 1, 0, 0, 1, 1 }, false), r, c);
 		board.scoreTile(r, c);
 		assertEquals(5, board.getPlayerScore(playerUp));
+		
+		r += 1;
+		board.addTile(new Tile(r, c, new int[] { 0, 1, 0, 1, 1 }, false), r, c);
+		board.scoreTile(r, c);
+		assertEquals(5, board.getPlayerScore(playerUp));
+		
+		r -= 2;
+		board.addTile(new Tile(r, c, new int[] { 1, 1, 1, 1, 0 }, true), r, c);
+		board.scoreTile(r, c);
+		assertEquals(9, board.getPlayerScore(playerUp));
+		
+		r -= 1;
+		board.addTile(new Tile(r, c, new int[] { 1, 1, 1, 1, 0 }, true), r, c);
+		board.scoreTile(r, c);
+		assertEquals(9, board.getPlayerScore(playerUp));
+		
+		r = board.CENTER;
+		c = board.CENTER - 1;
+		board.addTile(new Tile(r, c, new int[] { 0, 1, 1, 1, 0 }, true), r, c);
+		board.scoreTile(r, c);
+		assertEquals(14, board.getPlayerScore(playerUp));
+	}
+	
+	@Test
+	public void testScoreTileGameOn() {		
+		int playerUp = 0;
+		int r = board.CENTER;
+		int c = board.CENTER;
+		
+		board.scoreTile(r, c);
+		assertEquals(0, board.getPlayerScore(playerUp));
+		
+		c += 1;
+		board.addTile(new Tile(r, c, new int[] { 0, 1, 0, 1, 1 }, false), r, c);
+		// Confusing add tile
+		board.placeFollower(r, c, playerUp, 4);
+		board.scoreTile(r, c);
+		assertEquals(0, board.getPlayerScore(1));
+		assertEquals(0, board.getPlayerScore(2));
+		assertEquals(0, board.getPlayerScore(3));
+		assertEquals(0, board.getPlayerScore(4));
+		assertEquals(0, board.getPlayerScore(playerUp));
+		
+
+		c += 1;
+		board.addTile(new Tile(r, c, new int[] { 1, 0, 0, 1, 1 }, false), r, c);
+		board.scoreTile(r, c);
+		assertEquals(0, board.getPlayerScore(playerUp));
+		
+		r += 1;
+		board.addTile(new Tile(r, c, new int[] { 0, 1, 0, 1, 1 }, false), r, c);
+		board.scoreTile(r, c);
+		assertEquals(0, board.getPlayerScore(playerUp));
+		
+		r -= 2;
+		board.addTile(new Tile(r, c, new int[] { 1, 1, 1, 1, 0 }, true), r, c);
+		board.scoreTile(r, c);
+		assertEquals(0, board.getPlayerScore(playerUp));
+		
+		r -= 1;
+		board.addTile(new Tile(r, c, new int[] { 1, 1, 1, 1, 0 }, true), r, c);
+		board.scoreTile(r, c);
+		assertEquals(0, board.getPlayerScore(playerUp));
+		
+		r = board.CENTER;
+		c = board.CENTER - 1;
+		board.addTile(new Tile(r, c, new int[] { 0, 1, 1, 1, 0 }, true), r, c);
+		board.scoreTile(r, c);
+		assertEquals(5, board.getPlayerScore(playerUp));
+	}
+	
+	@Test
+	public void testGetNeighbor() {
+		int r = board.CENTER;
+		int c = board.CENTER;
+		
+		
+		c += 1;
+		board.addTile(new Tile(r, c, new int[] { 0, 1, 0, 1, 1 }, false), r, c);
+		board.placeFollower(r, c, 0, 4);
+
+		c += 1;
+		board.addTile(new Tile(r, c, new int[] { 1, 0, 0, 1, 1 }, false), r, c);
+		
+		r -= 1;
+		board.addTile(new Tile(r, c, new int[] { 1, 1, 1, 1, 0 }, true), r, c);
+		
+		r -= 1;
+		board.addTile(new Tile(r, c, new int[] { 1, 1, 1, 1, 0 }, true), r, c);
+		
+		r = board.CENTER;
+		c = board.CENTER - 1;
+		board.addTile(new Tile(r, c, new int[] { 0, 1, 1, 1, 0 }, true), r, c);
+		
+		assertNotNull(board.getNeighbor(0, board.getTile(72, 74)));
+		assertNull(board.getNeighbor(1, board.getTile(72, 74)));
+		assertNull(board.getNeighbor(2, board.getTile(72, 74)));
+		assertNotNull(board.getNeighbor(3, board.getTile(72, 74)));
 	}
 	
 	@Test
